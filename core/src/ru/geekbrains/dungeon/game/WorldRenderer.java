3fb7911fd5c70dda45d9d8824a466c2e4ef0248b
NabiyevTR <NabiyeTR@gmail.com>
2020-11-22 17:53:44 +0300

    hometask

--- core/src/ru/geekbrains/dungeon/game/WorldRenderer.java
+++ core/src/ru/geekbrains/dungeon/game/WorldRenderer.java
@@ -33,8 +33,9 @@ public class WorldRenderer {
         batch.draw(cursorTexture, gc.getCursorX() * GameMap.CELL_SIZE, gc.getCursorY() * GameMap.CELL_SIZE);
         batch.setColor(1, 1, 1, 1);
 
-        font24.draw(batch, "Player: " + gc.getUnitController().getHero().getName(), 20, 680);
-
+        font24.draw(batch, "Player: " + gc.getUnitController().getHero().getName(), 20, 690);
+        // Номер раунда на экране
+        font24.draw(batch, "Round: " + gc.getGameRound(), 20, 670);
         batch.end();
     }
 }
